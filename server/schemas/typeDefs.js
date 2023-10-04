2c3d86cf8e24ff065f66e71b6aa53a871832f079
Michie <contact@michiewillman.com>
2023-10-03 21:47:49 -0700

    Add book search input schema

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,14 +1,14 @@
 const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
-type User {
-  _id: ID!
-  username: String!
-  email: String!
-  password: String!
-  bookCount: Int
-  savedBooks: [Book]
-}
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    password: String!
+    bookCount: Int
+    savedBooks: [Book]
+  }
 
   type Book {
     bookId: ID!
@@ -19,12 +19,29 @@ type User {
     title: String!
   }
 
+  input BookSearch {
+    authors: [String]
+    description: String!
+    bookId: String!
+    image: String
+    link: String
+    title: String!
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
   type Query {
-    books: [Book]
-    users: [User]
+    me: User
   }
 
   type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookData: BookInput!): User
+    removeBook(bookId: ID!): User
   }
 `;
 
