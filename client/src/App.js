186bd1fd58dfed2ced3e13b8dd350fed7c92c9fc
Michie <contact@michiewillman.com>
2023-10-05 00:28:56 -0700

    Import dotenv

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,37 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+
+// Import pages
 import SearchBooks from "./pages/SearchBooks";
 import SavedBooks from "./pages/SavedBooks";
 import Navbar from "./components/Navbar";
 
-const client = new ApolloClient({
+// Create endpoint for API
+const graphLink = createHttpLink({
   uri: "/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  // Check local storage for authentication
+  const token = localStorage.getItem("id_token");
+  // Send found token back
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+// Use authentication on the endpoint
+const client = new ApolloClient({
+  link: authLink.concat(graphLink),
   cache: new InMemoryCache(),
 });
 
